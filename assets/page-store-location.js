bb60dbf0df7bbf787e12b7ab7a08256ee82edb67
Duong <nguyendinhduong2005yt@gmail.com>
2026-05-14 09:02:01 +0700

    first commit

--- assets/page-store-location.js
+++ assets/page-store-location.js
@@ -3,7 +3,8 @@ var map;
 var BlsCreateMapStoreLocation = (function () {
   return {
     init: function () {
-      mapboxgl.accessToken = "";
+      mapboxgl.accessToken =
+        "pk.eyJ1Ijoia2hpZW1waGFtIiwiYSI6ImNsam01eHhnaTAyNmczZmxzcnQ1MTVqN3gifQ.WVqIQlSk52FSN8W7G5gsnw";
       map = new mapboxgl.Map({
         container: "map",
         style: "mapbox://styles/mapbox/streets-v12",
